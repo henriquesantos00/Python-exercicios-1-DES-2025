387e4b6c601cfbd82d195e0e537d4e28f71c2883
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2025-05-29 14:54:58 +0000

    finalizando exercicio02

--- exercicio02.py
+++ exercicio02.py
@@ -3,3 +3,11 @@
 #  Se alguma tarefa estiver com o tempo de entrega negativo, deve-se exibir uma mensagem de 
 # erro e não somar o total.
 
+tarefaX = int(input("digite o tempo do em que foi feito a tarefaX:"))
+tarefaY = int(input("digite o tempo do em que foi feito a tarefaY:"))
+tarefaZ = int(input("digite o tempo do em que foi feito a tarefaZ:"))
+
+if tempo >= 10:
+    print("tarefa feita no praso.")
+else:
+   print("tempo de entrega atrasado.")
