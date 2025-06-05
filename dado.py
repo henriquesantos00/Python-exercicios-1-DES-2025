3e4ea4d7617bdb76cbe5b3f5137671cf44752c17
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2025-06-05 12:15:17 +0000

    dado.finalizado

--- dado.py
+++ dado.py
@@ -5,7 +5,7 @@ input("precione o enter para lançar o dado")
 resultado = random.randint(1,6)
 
 print(f"O dado rolou : {resultado}");
-if resultado > 6:
+if resultado > 4:
     print("ual! você é fera!")
 elif resultado < 6:
     print("tente novamente.")
