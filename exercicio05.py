31b9a012f7f2ddc3e6cc499ab998642e82284b8b
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2025-06-05 12:21:07 +0000

    fiinalizando.exercicio05

--- exercicio05.py
+++ exercicio05.py
@@ -1,8 +1,5 @@
 # Diego está acompanhando seu consumo de internet mensal, que tem um limite de 100 GB.
 # O programa deve receber o total consumido e avisar se ele ultrapassou o limite.
 
-umidade = int(input("digite a umidade no local:"))
-if umidade < 70:
-    print("umidade boa")
-elif umidade > 70:
-    print("alerta! umidade acima do limite")
+consumo = int(input("digite quanto foi o consumo mensal:"))
+if  consumo < 100gb:
