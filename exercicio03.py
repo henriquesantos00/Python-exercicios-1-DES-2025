c9fa7518c4c011fb237c14c024cb3759b38a535c
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2025-06-05 11:57:39 +0000

    exercicio03

--- exercicio03.py
+++ exercicio03.py
@@ -1,3 +1,8 @@
 # Rafael trabalha com armazenamento de grãos e precisa garantir que a umidade do ar no local não ultrapasse 70%.
 # Escreva um programa que receba o valor da umidade atual e exiba um alerta se estiver acima do limite.
 
+umidade = int(input("digite a umidade no local:"))
+if umidade < 70:
+    print("umidade boa")
+elif umidade > 70:
+    print("alerta! umidade acima do limite")
