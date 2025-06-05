85570908352be398a962fe94012397c087ea2ec8
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2025-06-05 12:08:26 +0000

    dado.py

---
+++ dado.py
@@ -0,0 +1,11 @@
+import random
+
+input("precione o enter para lançar o dado")
+
+resultado = random.randint(1,6)
+
+print(f"O dado rolou : {resultado}");
+if resultado > 6:
+    print("ual! você é fera!")
+elif resultado < 6:
+    print("tente novamente.")
