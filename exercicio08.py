cd044b0fd1f3aee7104dc3419c1d6ce627236d95
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2025-06-06 01:39:50 +0000

    exercicio08.finalizado

--- exercicio08.py
+++ exercicio08.py
@@ -4,4 +4,10 @@
 # De 51 a 150 km: R$ 15,00
 # Acima de 150 km: R$ 25,00
 
-
+distancia = float(input("digite a distancia percorrida:"))
+if distancia < 51:
+    print(" o valor foi R$ 5,00.")
+elif distancia < 150:
+    print("o valor foi R$ 15,00.")
+else:
+    print("o valor foi R$ 25,00.")
