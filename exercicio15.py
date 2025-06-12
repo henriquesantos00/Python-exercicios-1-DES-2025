739d49f4f0ae6c09e116e2f429ccf7b26565d2cf
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2025-06-12 14:45:48 +0000

    exercicio15

--- exercicio15.py
+++ exercicio15.py
@@ -1,3 +1,9 @@
 #Peça a idade do usuário e diga se ele pode se cadastrar em um site:
 
 #Permitido: 13 anos ou mais
+
+idade = int(input("digite a sua idade:"))
+if idade > 13:
+    print("acesso permitido.")
+else:
+    print("acesso negado.")
