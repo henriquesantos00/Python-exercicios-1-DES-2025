60dbd9e5e78ac9951412a21500b71899409ba80d
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2025-06-12 14:37:58 +0000

    exercicio10

--- exercicio010.py
+++ exercicio010.py
@@ -3,3 +3,12 @@
 # Salário mensal acima de R$ 3.000,00
 # A parcela não pode ser maior que 35% do salário
 
+salario = float(input("digite o salario:"))
+parcela = float(input("digite o valord da parcela do financiamento:"))
+
+if salario <= 3000:
+    print("financiamento negado, salário abaixo do minimo exigido")
+elif parcela > salario * 35:
+    print("financiamento negado, parcela acima de 35% do salário.")
+else:
+    print("financiamento aprovado!")
