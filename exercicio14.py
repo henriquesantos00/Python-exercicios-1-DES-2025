5fc69d2dd69728b1445f8c8f1a276ff10e8bb942
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2025-06-16 13:52:31 +0000

    exercicio14

--- exercicio14.py
+++ exercicio14.py
@@ -3,3 +3,12 @@
 #Compras acima de R$ 500,00 têm 10%
 #Acima de R$ 300,00 têm 5%
 #Menor ou igual a R$ 300,00 não têm desconto
+
+Compras = float(input("digite o valor da compra:"))
+
+if Compras >= 500:
+    print("você tem 10% de desconto")
+elif Compras <= 300:
+    print("você não tem desconto.")
+else:
+    print("você tem  5% de desconto.")
