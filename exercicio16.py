9a73d38dc033c031e4d8c444a7fcbd6ed4969f0b
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2025-06-16 14:03:48 +0000

    exercicio16

--- exercicio16.py
+++ exercicio16.py
@@ -3,3 +3,11 @@
 #Salários até R$ 2000,00: +15%
 #De R$ 2000,01 a R$ 5000,00: +10%
 #Acima de R$ 5000,00: +5%
+
+salario = float(input("digite seu salário:"))
+if salario <= 2000:
+    print("você recebera +15%.")
+elif salario <= 5000:
+    print("você recebera +10%.")
+else:
+    print("você recebera +5%.")
