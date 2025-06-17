476ba6d1e48817ef1838de83a985230033d45f9d
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2025-06-17 11:44:20 +0000

    carta

---
+++ carta.py
@@ -0,0 +1,6 @@
+destinatario = input("digite o nome do destinatário:")
+mensagem = input("digite sua mensagem:")
+remetente = input("digite seu nome:")
+
+print("\n--- Sua carta ---")
+print(f"olá {destinatario}, \n\n{mensagem}\n\nAtenciosamente, \n{remetente}")
