b81d1cd9c5e96c562474458223b69240f722812e
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2025-06-23 14:04:15 +0000

    exercicio13

--- exercicio13.py
+++ exercicio13.py
@@ -2,3 +2,8 @@
 #(Dica: múltiplos de 4, exceto os múltiplos de 100, a menos que também sejam múltiplos de 400)
 
 ano = int(input("digite um ano para saber se é bissexto:"))
+bissexto = ano % 4 == 0
+if bissexto == 0:
+    print("O ano não é bissexto. ")
+else:
+    print("o ano é bissexto.")
