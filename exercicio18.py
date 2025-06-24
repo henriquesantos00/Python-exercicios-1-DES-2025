d9e4dbded5db7afaa7434290a33d9a08a2acb362
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2025-06-24 12:18:49 +0000

    exercicio18

--- exercicio18.py
+++ exercicio18.py
@@ -1,3 +1,10 @@
 #Receba duas notas e seus respectivos pesos. Calcule a média ponderada.
 #Fórmula: (nota1 × peso1 + nota2 × peso2) / (peso1 + peso2)
 
+nota1 = int(input("digite a nota 1: "))
+peso1 = int(input("digite o peso 1: "))
+nota2 = int(input("digite a nota 2: "))
+peso2 = int(input("digite o peso 2: "))
+
+f = (nota1 * peso1 + nota2 * peso2) / (peso1 + peso2)
+print(f"Média: {f}")
